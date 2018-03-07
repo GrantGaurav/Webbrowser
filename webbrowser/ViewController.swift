e7ec902311d0ee4e16f20241856bd82315e680e5
Gaurav-Semwal <grant.gaurav@ithands.biz>
2018-03-07 11:01:23 +0530

    Complete Project

--- webbrowser/ViewController.swift
+++ webbrowser/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var myWebView: UIWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let url = URL(string : "http://www.google.com")
+        myWebView.loadRequest(URLRequest(url: url!))
     }
 
     override func didReceiveMemoryWarning() {
